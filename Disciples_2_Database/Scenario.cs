78aa97185700bc725907ad2d40f426913a24f9b3
WizMe-M <enderman.2016@mail.ru>
2022-01-12 17:08:42 +0300

    Initial commit

---
+++ Disciples_2_Database/Scenario.cs
@@ -0,0 +1,10 @@
+﻿namespace Disciples_2_Database;
+
+public class Scenario
+{
+    public string Id { get; set; } 
+    public string Info_Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<string> Brieflongs { get; set; }
+}
